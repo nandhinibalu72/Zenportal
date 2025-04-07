0c36aad744c3996550de00be9d3d455d267ffc4a
nandhinibalu72 <nandhinibalu72@gmail.com>
2025-04-08 00:16:17 +0530

    Added test data

---
+++ TestData/data.py
@@ -0,0 +1,12 @@
+"""
+Stored all the data in data.py
+"""
+
+class ZenData:
+    Zen_url = "https://v2.zenclass.in/login"
+    username = "nandhinibalu72@gmail.com"
+    password = "Nivan@10"
+
+    # negative test data
+    invalid_username="admin@gmail.com"
+    invalid_password = "admin"
